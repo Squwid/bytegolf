a2f93f0432a78c1234ead83d54b497230205b639
Squwid <bwhitelaw@sbcglobal.net>
2019-09-12 19:46:11 -0400

    first start of angular frontend

--- go.mod
+++ go.mod
@@ -1,15 +1,22 @@
 module github.com/Squwid/bytegolf
 
 require (
+	cloud.google.com/go v0.45.1
 	github.com/aws/aws-sdk-go v1.19.15
+	github.com/go-redis/redis v6.15.5+incompatible
+	github.com/google/uuid v1.1.1
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/kr/pretty v0.1.0
 	github.com/lib/pq v1.1.1
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/satori/go.uuid v1.2.0
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529
-	golang.org/x/net v0.0.0-20190509222800-a4d6f7feada5 // indirect
-	golang.org/x/sys v0.0.0-20190509141414-a5b02f93d862 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20190510212250-80b1e8742cb0 // indirect
+	golang.org/x/crypto v0.0.0-20190605123033-f99c8df09eb5
+	golang.org/x/sys v0.0.0-20190907184412-d223b2b6db03 // indirect
+	google.golang.org/api v0.9.0
+	google.golang.org/appengine v1.6.1
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
+
+go 1.13
--- main.go
+++ main.go
@@ -1,104 +1,34 @@
 package main
 
 import (
-	"crypto/tls"
-	"html/template"
-	"log"
 	"net/http"
-	"os"
-	"time"
 
-	"github.com/Squwid/bytegolf/database"
-	_ "github.com/Squwid/bytegolf/questions"
-	"github.com/Squwid/bytegolf/users"
-	"github.com/aws/aws-sdk-go/aws"
-	awss "github.com/aws/aws-sdk-go/aws/session"
-	"golang.org/x/crypto/acme/autocert"
+	_ "github.com/Squwid/bytegolf/firestore"
+	"github.com/go-redis/redis"
 )
 
 var siteAddr = "https://bytegolf.io"
-var tpl *template.Template
-var awsSess *awss.Session
 
-// Loggers
-var (
-	logger *log.Logger
-)
-
-type code struct {
-	Show    bool
-	Correct bool
-	Bytes   int64
-	Output  string
-}
+const compilerURI = "http://jorts/compile"
 
 func init() {
-	tpl = template.Must(template.ParseGlob("views/*"))
-	logger = log.New(os.Stdout, "[bytegolf] ", log.Ldate|log.Ltime)
-	awsSess = awss.Must(awss.NewSessionWithOptions(awss.Options{Config: aws.Config{Region: aws.String("us-east-1")}}))
+	rdsClient = redis.NewClient(&redis.Options{
+		Addr:     "redis:80",
+		Password: "",
+		DB:       0,
+	})
 }
 
 func main() {
-	if database.InProd() {
-		mux := http.NewServeMux()
-		certManager := autocert.Manager{
-			Prompt: autocert.AcceptTOS,
-			Cache:  autocert.DirCache("cert-cache"),
-		}
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 
-		mux.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	// handlers
+	http.HandleFunc("/", index)
+	http.HandleFunc("/testing", sess)
+	http.HandleFunc("/compile", compile)
+	http.ListenAndServe(":80", nil)
+}
 
-		// handlers
-		mux.HandleFunc("/", index)
-		mux.HandleFunc("/play/", play)
-		mux.HandleFunc("/submit/", submission)
-		mux.HandleFunc("/holes/", holes)
-		mux.HandleFunc("/login", users.GitLogin)
-		mux.HandleFunc("/login/check", users.GithubOAUTH)
-		mux.HandleFunc("/account/", account)
-		mux.HandleFunc("/leaderboards", leaderboards)
-
-		/* ADMIN FUNCTIONS */
-		mux.HandleFunc("/admin/", admin)
-		mux.HandleFunc("/admin/archive/", archiveQuestion)
-		mux.HandleFunc("/admin/deploy/", deployQuestion)
-		mux.HandleFunc("/admin/addhole", createQuestion)
-		mux.HandleFunc("/admin/delete/", deletehole)
-		mux.HandleFunc("/admin/holes", adminholes)
-
-		// listen and serve
-		server := &http.Server{
-			ReadTimeout:  5 * time.Second,
-			WriteTimeout: 5 * time.Second,
-			IdleTimeout:  120 * time.Second,
-			Addr:         ":443",
-			Handler:      mux,
-			TLSConfig: &tls.Config{
-				GetCertificate: certManager.GetCertificate,
-			},
-		}
-		go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
-		server.ListenAndServeTLS("", "")
-	} else {
-		http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
-
-		// handlers
-		http.HandleFunc("/", index)
-		http.HandleFunc("/play/", play)
-		http.HandleFunc("/submit/", submission)
-		http.HandleFunc("/holes/", holes)
-		http.HandleFunc("/login", users.GitLogin)
-		http.HandleFunc("/login/check", users.GithubOAUTH)
-		http.HandleFunc("/account/", account)
-		http.HandleFunc("/leaderboards", leaderboards)
-
-		/* ADMIN FUNCTIONS */
-		http.HandleFunc("/admin/", admin)
-		http.HandleFunc("/admin/archive/", archiveQuestion)
-		http.HandleFunc("/admin/deploy/", deployQuestion)
-		http.HandleFunc("/admin/addhole", createQuestion)
-		http.HandleFunc("/admin/delete/", deletehole)
-		http.HandleFunc("/admin/holes", adminholes)
-		http.ListenAndServe(":80", nil)
-	}
+func index(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("byte golf api"))
 }
